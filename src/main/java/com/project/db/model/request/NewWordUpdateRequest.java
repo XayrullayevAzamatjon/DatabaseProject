cb7e040186f62cb56efebac643aa1219bff7bb1a
nodiraAxmedova <nodiraaxmedova0502@gmail.com>
2024-05-07 17:34:27 +0500

    fixed varibles

--- src/main/java/com/project/db/model/request/NewWordUpdateRequest.java
+++ src/main/java/com/project/db/model/request/NewWordUpdateRequest.java
@@ -1,7 +1,10 @@
 package com.project.db.model.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.project.db.utils.Part_of_speech;
 
-public record NewWordUpdateRequest(String word_id, String written_form,
-                                   Part_of_speech part_of_speech) {
+public record NewWordUpdateRequest(
+        @JsonProperty(namespace = "word_id") String wordId,
+        @JsonProperty(namespace = "written_form") String writtenForm,
+        @JsonProperty(namespace = "part_of_speech") Part_of_speech partOfSpeech) {
 }
