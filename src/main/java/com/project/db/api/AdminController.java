72b68206d112082b0d7045cdb98ecc09d3694764
nodiraAxmedova <nodiraaxmedova0502@gmail.com>
2024-05-08 16:26:27 +0500

    changeStatusToConfirmed function is written

--- src/main/java/com/project/db/api/AdminController.java
+++ src/main/java/com/project/db/api/AdminController.java
@@ -2,7 +2,9 @@ package com.project.db.api;
 
 import com.project.db.model.response.NewWordResponse;
 import com.project.db.service.AdminService;
+import com.project.db.utils.Status;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,4 +25,8 @@ public class AdminController {
         return adminService.findAllRequestedWords();
     }
 
+    @GetMapping("/confirm/{newWordId}")
+    public Status confirmNewWord(@PathVariable String newWordId){
+        return adminService.confirmNewWord(newWordId);
+    }
 }
